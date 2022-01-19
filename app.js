83f29ad23ce18c653f9f79dd5e3222fc0282236e
anastassiya.savostina <a.savostina@icloud.com>
2022-01-18 18:37:50 -0700

    done

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-document.body.style.fontFamily ='Arial, sans-serif';
+// document.body.style.fontFamily ='Arial, sans-serif';
 
-document.getElementById('nickname').textContent ='Nastenok';
-document.getElementById('favorites').textContent ='music, reading';
-document.getElementById('hometown').textContent ='Balkhash';
+// document.getElementById('nickname').textContent ='Nastenok';
+// document.getElementById('favorites').textContent ='music, reading';
+// document.getElementById('hometown').textContent ='Balkhash';
 
-var items = document.getElementsByTagName('li');
-for (var i =0; i < items.length; i++) {
-    items[i].className = 'list-item';
+// var items = document.getElementsByTagName('li');
+// for (var i =0; i < items.length; i++) {
+//     items[i].className = 'list-item';
+// }
+// var myPic =document.createElement('img');
+// myPic.src = 'https://www.google.com/aclk?sa=L&ai=DChcSEwig0sfUgbD1AhV6fG8EHa_6CDYYABAHGgJqZg&sig=AOD64_0Q0w4Irjdo6mdyobLww_msLmWgVQ&adurl&ctype=5&ved=2ahUKEwjHkbfUgbD1AhUOXs0KHeBKCTYQvhd6BAgBEHc';
+// document.body.appendChild(myPic);
+
+var books = [
+    {
+        title: 'The design of EveryDay Things',
+        author: 'Don Norman',
+
+    }, {
+        title: 'The Most Human Human',
+        author: 'Brian Christian',
+        alreadyRead: true
+    }
+];
+for ( var i  = 0; i< books.length; i++) {
+    var bookP = document.createElement('p');
+    var bookDescription = document.createTextNode(books[i].title+ 'by'+ books[i].author);
+    bookP.appendChild(bookDescription);
+    document.body.appendChild(bookP);
+
 }
-var myPic =document.createElement('img');
-myPic.src = 'https://www.google.com/aclk?sa=L&ai=DChcSEwig0sfUgbD1AhV6fG8EHa_6CDYYABAHGgJqZg&sig=AOD64_0Q0w4Irjdo6mdyobLww_msLmWgVQ&adurl&ctype=5&ved=2ahUKEwjHkbfUgbD1AhUOXs0KHeBKCTYQvhd6BAgBEHc';
-document.body.appendChild(myPic);
-
 
 
--- currency.js
+++ currency.js
@@ -1,39 +1,3 @@
-// const apiURL= "//currency-exchange.p.rapidapi.com/"
-
-// fetch (apiURL)
-// .then((response) => response.json())
-// .then((currencyInfo) => {
-//       console.log(currencyInfo);
-  
-
-      
-      
-//       document.getElementById("title").innerHTML=currencyInfo.Title;
-//       document.getElementById("year").innerHTML=currencyInfo.Year;
-//     });      
-     
-
-
-// document.getElementById("place").innerHTML=weatherInfo.name;
-// document.getElementById("feels").innerHTML=weatherInfo.main.feels_like;
-// document.getElementById("currentTemp").innerHTML=weatherInfo.main.temp;
-// document.getElementById("windSped").innerHTML=weatherInfo.wind.speed;
-// document.getElementById("humid").innerHTML=weatherInfo.main.humidity;
-// document.getElementById("sunny").innerHTML=weatherInfo.sys.sunrise;
-
-
-
-// const iconcode=weatherInfo.weather[0].icon;
-// console.log(iconcode);
-// const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
-// console.log(icon_path);
-
-// document.getElementById("weatherIcon").src=icon_path;
-// });
-
-
-
-
 // include api for currency change
 const api = "https://api.exchangerate-api.com/v4/latest/USD";
 
@@ -88,5 +52,5 @@ function displayResults(currency) {
 // when user click on reset button
 function clearVal() {
 	window.location.reload();
-	document.getElementsByClassName("finalValue").innerHTML = "";
+	document.getElementById("finalValue").innerHTML = "";
 };
